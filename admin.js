07858381947bc2383de9c8b8b5b8917a9ee31889
colegiojapi-glitch <colegiojapi@gmail.com>
2026-06-11 11:44:31 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -9,6 +9,23 @@ window.supabase.createClient(
 SUPABASE_URL,
 SUPABASE_KEY
 );
+(async () => {
+
+const {
+  data: { session }
+} =
+await supabaseClient.auth.getSession();
+
+if (!session) {
+
+  window.location.href =
+  "login.html";
+
+  return;
+
+}
+
+})();
 
 const lista =
 document.getElementById(
